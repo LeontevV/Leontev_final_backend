b782476f5801707a8ec0104db7b6812ea7f5efea
LeontevV <v.leontev@dunice.net>
2022-05-24 12:44:42 +0300

    delete unnecessary

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ Rails.application.routes.draw do
                 sessions: 'users/sessions',
                 registrations: 'users/registrations'
               }
-  get '/member/:id', to: 'members#show'
-  # patch '/member/:id', to: 'members#update'
   resources :posts
   get '/posts/:user_id', to: 'posts#show'
-  # post '/posts', to: 'posts#create'
-  put '/users/:id', to: 'users#update'
 end
