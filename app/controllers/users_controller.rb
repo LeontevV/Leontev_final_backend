4b7016f407953823582a03036bbd696ef049d843
LeontevV <v.leontev@dunice.net>
2022-06-24 16:51:07 +0300

    users+members controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
   
+  private
+
+  def show
+    render json: @user.to_json(include: :posts)
+  end
+  
   def set_user
     @user = User.find(params[:id])
-  end  
+  end
+ 
 end
